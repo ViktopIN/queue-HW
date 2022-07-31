f63d64d6f2174ddcd2c28d023b569ade26cb940b
Viktor <viktopiniaginya@gmail.com>
2022-07-31 08:25:21 +0300

    setup view controller

--- queue-HW/Sources/Scenes/Controller/ViewController.swift
+++ queue-HW/Sources/Scenes/Controller/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+//  MARK: - Properties
+    private var mainView: View? {
+        guard isViewLoaded else { return nil }
+        return view as? View
+    }
+        
+    var isBlack: Bool = false {
+        didSet {
+            if isBlack {
+                self.view.backgroundColor = .black
+            } else {
+                self.view.backgroundColor = .white
+                }
+        }
+    }
+//  MARK: - Methods
+    @IBAction func onBut(_ sender: UIButton) {
+        isBlack.toggle()
+    }
+        
+    func bruteForce(passwordToUnlock: String) {
+        let ALLOWED_CHARACTERS:   [String] = String().printable.map { String($0) }
 
+        var password: String = ""
+
+        // Will strangely ends at 0000 instead of ~~~
+        while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
+            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+//             Your stuff here
+            print(password)
+            // Your stuff here
+        }
+            
+        print(password)
+    }
+//  MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            
+        mainView?.textField.isSecureTextEntry = true
+            
+        self.bruteForce(passwordToUnlock: "1!gr")
     }
-
-
 }
+
 
