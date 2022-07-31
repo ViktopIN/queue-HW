1d837bc4990859276a7be57abf3514d0e869318e
Viktor <viktopiniaginya@gmail.com>
2022-07-31 08:12:55 +0300

    setup view

--- queue-HW/Sources/Scenes/View/View.swift
+++ queue-HW/Sources/Scenes/View/View.swift
@@ -8,13 +8,31 @@
 import UIKit
 
 class View: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+//    MARK: - Properties
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var passwordButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var colorButton: UIButton!
+//    MARK: - Initialize
+    init() {
+        super.init(frame: .zero)
+        configure()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        textField.isSecureTextEntry = true
+        
+        label.textAlignment = .center
+        label.text = "Password is ..."
+        
+        colorButton.titleLabel?.text = "Change color"
+        
+        passwordButton.titleLabel?.text = "Selection password"
+    }
 
 }
