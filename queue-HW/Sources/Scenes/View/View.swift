fd0849d1a977132e5f13832a80202c88054db74d
Viktor <viktopiniaginya@gmail.com>
2022-07-31 08:10:37 +0300

    create folders

---
+++ queue-HW/Sources/Scenes/View/View.swift
@@ -0,0 +1,20 @@
+//
+//  View.swift
+//  queue-HW
+//
+//  Created by Виктор on 31.07.2022.
+//
+
+import UIKit
+
+class View: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
